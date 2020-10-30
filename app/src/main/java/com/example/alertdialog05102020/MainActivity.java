3288561c475f18520cfae39d95228624fb021418
phatdroid94 <phatdroid94@gmail.com>
2020-10-30 21:16:30 +0700

    alert dialog

--- app/src/main/java/com/example/alertdialog05102020/MainActivity.java
+++ app/src/main/java/com/example/alertdialog05102020/MainActivity.java
@@ -6,6 +6,7 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -13,10 +14,14 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     Button mBtnAlertDialog;
+    int mPosition;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mPosition = -1;
 
         mBtnAlertDialog = findViewById(R.id.buttonAlert);
 
@@ -25,16 +30,32 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                builder.setTitle("Bạn có thông báo mới");
-                builder.setMessage("Phiên bản đã có bản cập nhật");
+                builder.setTitle("Hãy chọn tên con vật");
                 builder.setIcon(R.mipmap.ic_launcher);
                 builder.setCancelable(false);
                 // Single choice , multiple choice
 
+                final String[] arrAnimals = {"Mèo", "Chó", "Voi", "Gà", "Khỉ", "Heo"};
+                final boolean[] arrSelected = {false, false, false, false, false, false};
+
+
+                builder.setMultiChoiceItems(arrAnimals, arrSelected, new DialogInterface.OnMultiChoiceClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+
+                    }
+                });
                 builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
+                        String result = "";
+                        for (int i = 0; i < arrSelected.length ; i++) {
+                            if (arrSelected[i]){
+                                result += arrAnimals[i] + " , ";
+                            }
+                        }
+                        result = result.substring(0, result.length() - 3);
+                        Toast.makeText(MainActivity.this, result, Toast.LENGTH_SHORT).show();
                     }
                 });
                 builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
